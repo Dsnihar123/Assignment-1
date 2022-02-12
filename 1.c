d1ee98f313035463b771adf6f6957d7c1b53c307
Dsnihar123 <97696380+Dsnihar123@users.noreply.github.com>
2022-02-12 20:25:01 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,12 @@
+//C program to display basic information
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+  printf("Name:Nihar Ranjan Das\n ");
+  printf("Branch: Information Technology\n");
+  printf("Regno:2002081065\n");
+  printf("Hobbies: Playing Cricket, Reading Books");
+  getchar();
+}
---
+++ 2.c
@@ -0,0 +1,16 @@
+//Find maximum number between two
+//#include <stdio.h>
+int main()
+{
+  
+int  a,b,max;
+  printf("Enter two numbers");
+  scanf("%d %d",&a,&b);
+  if(a>b)
+      max=a;
+  else
+      max=b;
+  printf("max is %d",max);
+  return 0;
+  
+}
